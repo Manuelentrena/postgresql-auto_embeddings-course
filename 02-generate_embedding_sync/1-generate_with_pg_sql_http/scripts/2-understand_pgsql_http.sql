446bfbd4e4ea340973745248164989cf3f605e90
EntrenaDev <manuel.entrena@gmail.com>
2026-04-15 13:25:21 +0200

    Refactor SQL script to use OpenAI API for embeddings and update HTTP request format

--- 02-generate_embedding_sync/1-generate_with_pg_sql_http/scripts/2-understand_pgsql_http.sql
+++ 02-generate_embedding_sync/1-generate_with_pg_sql_http/scripts/2-understand_pgsql_http.sql
@@ -1,11 +1,19 @@
-CREATE EXTENSION IF NOT EXISTS "http";
+CREATE EXTENSION IF NOT EXISTS http;
+
+SET my.openai.key = 'MY KEY';
 
 SELECT *
-FROM http_post(
-	'http://1-generate_with_pg_sql_http-ollama-1:11434/api/embeddings',
-	JSONB_BUILD_OBJECT(
-		'model', 'nomic-embed-text',
-		'prompt', 'Un curso muy guapo'
-	)::text,
-	'application/json'
- );
+FROM http((
+  'POST'::text,
+  'https://api.openai.com/v1/embeddings'::text,
+  ARRAY[
+    http_header('Authorization', 'Bearer ' || current_setting('my.openai.key'))
+  ]::http_header[],
+  'application/json'::text,
+  jsonb_build_object(
+    'model', 'text-embedding-3-small',
+    'input', 'Un curso muy guapo',
+    'dimensions', 768
+  )::text
+)::http_request);
+
