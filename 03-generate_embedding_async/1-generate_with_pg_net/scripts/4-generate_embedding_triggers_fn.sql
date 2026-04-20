57a2624372dbf3dd40095d4b5a7f2f3d755c9770
EntrenaDev <manuel.entrena@gmail.com>
2026-04-20 10:19:06 +0200

    Refactor embedding trigger function to correct JSON path and improve structure

--- 03-generate_embedding_async/1-generate_with_pg_net/scripts/4-generate_embedding_triggers_fn.sql
+++ 03-generate_embedding_async/1-generate_with_pg_net/scripts/4-generate_embedding_triggers_fn.sql
@@ -1,38 +1,6 @@
+create extension if not exists pg_net;
+
 ALTER TABLE mooc.courses ADD COLUMN request_id BIGINT;
-
-CREATE OR REPLACE FUNCTION generate_embedding(
-)
-	RETURNS TRIGGER
-	LANGUAGE plpgsql
-AS
-$$
-DECLARE
-	embedding_input_func_name TEXT = tg_argv[0];
-	query_string TEXT;
-	text_content TEXT;
-	request_id BIGINT;
-	api_url TEXT := 'http://1-generate_with_pg_net-ollama-1:11434/api/embeddings';
-BEGIN
-	query_string := 'SELECT ' || embedding_input_func_name || '($1)';
-	EXECUTE query_string INTO text_content USING new;
-
-	SELECT net.http_post(
-		url := api_url,
-		body := JSONB_BUILD_OBJECT(
-			'model', 'nomic-embed-text',
-			'prompt', text_content
-		),
-		headers := JSONB_BUILD_OBJECT('Content-Type', 'application/json')
-	)
-	INTO request_id;
-
-	RAISE WARNING 'Request ID: %', request_id;
-
-	new.request_id = request_id;
-
-	RETURN new;
-END;
-$$;
 
 CREATE OR REPLACE FUNCTION net._http_response__handle_embedding_response(
 )
@@ -45,7 +13,7 @@ DECLARE
 BEGIN
 	SELECT ARRAY_AGG(e::DOUBLE PRECISION)
 	INTO embedding_array
-	FROM JSONB_ARRAY_ELEMENTS_TEXT(new.content::jsonb -> 'embedding') AS e;
+	FROM jsonb_array_elements_text(new.content::jsonb -> 'data' -> 0 -> 'embedding') AS e;
 
 	UPDATE mooc.courses
 	SET embedding = embedding_array::vector
