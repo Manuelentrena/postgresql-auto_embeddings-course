b2a1fe8958f675b614610fb0d697c6848a854147
EntrenaDev <manuel.entrena@gmail.com>
2026-04-20 11:48:39 +0200

    Add functions and table for handling embedding requests and responses

---
+++ 03-generate_embedding_async/2-generate_with_custom_endpoint/databases/1-utils.sql
@@ -0,0 +1,73 @@
+CREATE TABLE net.embedding_requests (
+	request_id BIGINT NOT NULL,
+	table_name TEXT NOT NULL
+);
+
+CREATE OR REPLACE FUNCTION generate_embedding(
+)
+	RETURNS TRIGGER
+	LANGUAGE plpgsql
+AS
+$$
+DECLARE
+	embedding_input_func_name TEXT = tg_argv[0];
+	query_string TEXT;
+	text_content TEXT;
+	request_id BIGINT;
+	table_name TEXT = tg_table_name;
+	schema_name TEXT = tg_table_schema;
+	api_url TEXT := 'https://api.openai.com/v1/embeddings';
+BEGIN
+	query_string := 'SELECT ' || embedding_input_func_name || '($1)';
+	EXECUTE query_string INTO text_content USING new;
+
+	-- Llamada correcta a OpenAI
+	SELECT net.http_post(
+		url := api_url,
+		body := JSONB_BUILD_OBJECT(
+			'model', 'text-embedding-3-small',
+			'input', text_content,
+      'dimensions', 768
+		),
+		headers := JSONB_BUILD_OBJECT(
+			'Content-Type', 'application/json',
+			'Authorization', 'Bearer YOUR_OPENAI_API_KEY'
+		)
+	)
+	INTO request_id;
+
+  INSERT INTO net.embedding_requests (request_id, table_name) VALUES (request_id, schema_name || '.' || table_name);
+
+	new.request_id = request_id;
+
+	RETURN new;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION net._http_response__handle_embedding_response(
+)
+	RETURNS TRIGGER
+	LANGUAGE plpgsql
+AS
+$$
+DECLARE
+	embedding_array FLOAT[];
+	table_name_with_schema TEXT;
+BEGIN
+	SELECT ARRAY_AGG(e::DOUBLE PRECISION)
+	INTO embedding_array
+	FROM jsonb_array_elements_text(new.content::jsonb -> 'data' -> 0 -> 'embedding') AS e;
+
+	SELECT table_name FROM net.embedding_requests WHERE request_id = new.id INTO table_name_with_schema;
+
+	EXECUTE FORMAT('UPDATE %s SET embedding = $1::vector WHERE request_id = $2', table_name_with_schema)
+		USING embedding_array, new.id;
+	RETURN new;
+END;
+$$;
+
+CREATE OR REPLACE TRIGGER trg__http_response__handle_embedding_response_after_insert
+	AFTER INSERT
+	ON net._http_response
+	FOR EACH ROW
+EXECUTE FUNCTION net._http_response__handle_embedding_response();
